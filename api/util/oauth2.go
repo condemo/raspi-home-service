828d9395771548b5d4aebda609308f5fa25fb89c
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-05-28 17:56:57 +0200

    feat: jwt utilities

---
+++ api/util/oauth2.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type UserClaims struct {
+	jwt.RegisteredClaims
+	id uint8
+}
+
+func CreateJWT(id uint8) (string, error) {
+	jwtKey := []byte(os.Getenv("JWT_KEY"))
+
+	expire, _ := strconv.Atoi(os.Getenv("JWT_EXP_DAYS"))
+	expDays := time.Now().Add(time.Hour * 24 * time.Duration(expire))
+
+	claims := UserClaims{
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expDays),
+		},
+		id,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(jwtKey)
+
+	return signedToken, err
+}
+
+func ValidateJWT(t string) (*UserClaims, error) {
+	parsedToken, err := jwt.ParseWithClaims(
+		t, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_KEY")), nil
+		})
+	return parsedToken.Claims.(*UserClaims), err
+}
