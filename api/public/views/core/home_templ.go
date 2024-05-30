dec3375f4971f14f74a29ed38d2dc3691efc0d6b
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-05-30 19:10:02 +0200

    added css load

--- api/public/views/core/home_templ.go
+++ api/public/views/core/home_templ.go
@@ -31,7 +31,7 @@ func Home(data map[string]string) templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h2>WORKING</h2>")
+			templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 1)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
