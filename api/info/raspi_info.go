39fa37c85abf523cff17003f2aad7969f235d7fe
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-06-01 22:52:45 +0200

    adapt json tags to raspi structure

--- api/info/raspi_info.go
+++ api/info/raspi_info.go
@@ -9,13 +9,13 @@ type Rasp5Temp struct {
 	Cpu struct {
 		Tctl struct {
 			Temp float32 `json:"temp1_input"`
-		} `json:"tctl"`
-	} `json:"k10temp-pci-00c3"`
-	Gpu struct {
-		Edge struct {
-			Temp float32 `json:"temp1_input"`
-		} `json:"edge"`
-	} `json:"amdgpu-pci-0400"`
+		} `json:"temp1"`
+	} `json:"cpu_thermal-virtual-0"`
+	Fan struct {
+		Fan1 struct {
+			Speed float32 `json:"fan1_input"`
+		} `json:"fan1"`
+	} `json:"pwmfan-isa-0000"`
 }
 
 func RaspberryTemps() *Rasp5Temp {
