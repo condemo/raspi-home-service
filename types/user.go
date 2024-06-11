cf4f0add7a8474983f5ea6c3a124f8938184c6bd
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-06-11 20:02:52 +0200

    feat: simple user credentials validator added

--- types/user.go
+++ types/user.go
@@ -1,10 +1,31 @@
 package types
 
+import (
+	"unicode"
+)
+
 type User struct {
 	Username string `gorm:"primaryKey,unique" json:"username"`
 	Password string `json:"-"`
 	ID       uint8  `json:"id"`
 }
 
-// TODO: Implementar
-func (u User) Validate() bool { return true }
+// PERF: Mejorar usando un mejor validator
+func (u User) Validate() bool {
+	if u.Password == "" || u.Username == "" {
+		return false
+	}
+
+	for _, c := range u.Username {
+		if unicode.IsSpace(c) || unicode.IsSymbol(c) {
+			return false
+		}
+	}
+	for _, c := range u.Password {
+		if unicode.IsSpace(c) || unicode.IsSymbol(c) {
+			return false
+		}
+	}
+
+	return true
+}
