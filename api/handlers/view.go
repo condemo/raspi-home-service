1da74d9f19958ea0a773e23d55d19ba8edb3e0ab
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-06-19 20:18:23 +0200

    basic theme swap handler

--- api/handlers/view.go
+++ api/handlers/view.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/condemo/raspi-home-service/config"
 	"github.com/condemo/raspi-home-service/store"
 	"github.com/condemo/raspi-home-service/tools"
 	"github.com/condemo/raspi-home-service/views/core"
@@ -18,9 +20,19 @@ func NewViewHanlder(s store.Store) *ViewHandler {
 
 func (h *ViewHandler) RegisterRoutes(r *http.ServeMux) {
 	r.HandleFunc("GET /", h.homeHandler)
+	r.HandleFunc("POST /theme", h.themeChange)
 }
 
 func (h *ViewHandler) homeHandler(w http.ResponseWriter, r *http.Request) {
 	sysInfo := tools.NewSysInfo()
 	RenderTempl(w, r, core.Home(sysInfo))
+}
+
+func (h *ViewHandler) themeChange(w http.ResponseWriter, r *http.Request) {
+	t := r.URL.Query().Get("t")
+	i, _ := strconv.Atoi(t)
+
+	config.UIConf.ChangeTheme(i)
+
+	http.Redirect(w, r, "/", http.StatusPermanentRedirect)
 }
