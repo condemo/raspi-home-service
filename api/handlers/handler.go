c69d997a3e71a89e436d6cf6f1d69ee82ae84f38
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-06-20 16:08:26 +0200

    refactor: `HTTPSendHTML` utility

--- api/handlers/handler.go
+++ api/handlers/handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -11,6 +12,15 @@ import (
 
 func RenderTempl(w http.ResponseWriter, r *http.Request, c templ.Component) {
 	c.Render(r.Context(), w)
+}
+
+func HTTPSendHTML(w http.ResponseWriter, c templ.Component) {
+	tmpl, err := templ.ToGoHTML(context.Background(), c)
+	if err != nil {
+		ErrorLog(w, http.StatusInternalServerError, "internal server err")
+	}
+
+	fmt.Fprint(w, tmpl)
 }
 
 func ErrorLog(w http.ResponseWriter, status int, msg string) {
