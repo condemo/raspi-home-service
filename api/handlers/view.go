f14c6e0187f20638526e84f586626cd4f03b2ff4
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-06-23 19:08:27 +0200

    feat: `/config` route handler

--- api/handlers/view.go
+++ api/handlers/view.go
@@ -18,9 +18,14 @@ func NewViewHanlder(s store.Store) *ViewHandler {
 
 func (h *ViewHandler) RegisterRoutes(r *http.ServeMux) {
 	r.HandleFunc("GET /", h.homeHandler)
+	r.HandleFunc("GET /config", h.configHandler)
 }
 
 func (h *ViewHandler) homeHandler(w http.ResponseWriter, r *http.Request) {
 	sysInfo := tools.NewSysInfo()
 	RenderTempl(w, r, core.Home(sysInfo))
+}
+
+func (h *ViewHandler) configHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTempl(w, r, core.Config())
 }
